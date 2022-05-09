b638c9b703927580d9e3aef2adceff9ea660f9ab
alinivan <ai626@live.mdx.ac.uk>
2022-05-09 20:49:47 +0100

    improved ML for number recognition, added image rotation and dynamic crop for live stream

--- image_processing.py
+++ image_processing.py
@@ -9,60 +9,112 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from keras.preprocessing.image import load_img as load 
 from sklearn.utils import Bunch
 
-cardColors = ["r", "y", "g", "b"]
+cardColors = ["r", "g", "b", "y"]
+colors = ["RED","GREEN", "BLUE", "YELLOW"]
 cardNumbers = [0,1,2,3,4,5,6,7,8,9,"d","a","n"]
 colorClf = KNeighborsClassifier(2)
 numberClf = KNeighborsClassifier(2)
-resX = 800
-resY = 600
+resX = 600
+resY = 800
+
+#get contours / args: numpy array - image / return numpy array - contours
+def getContours(frame):
+   #contours of card in frame
+   mono = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+   blur = cv.blur(mono, (10, 10))
+   th = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 35, 2)
+   kernel = np.ones((5, 5), np.uint8)
+   close = cv.morphologyEx(th, cv.MORPH_OPEN, kernel)
+   #canny = cv.Canny(close, 50, 100)
+   cont, t = cv.findContours(close, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+   return cont
+
+def getThreshold(frame):
+    mono = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    blur = cv.blur(mono, (5, 5))
+    th = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 35, 2)
+    return th
 
 #get card color / args: KNN - classifier, numpy array - card / return: string - color
 def getCardColor(clf, card):
-    color = "color "
-    #prediction = clf.predict(card.data)
-    #color += cardColors[prediction[0]]
+    color = ""
+    prediction = clf.predict(card.data)
+    color += colors[prediction[0]]
     return color
 
 #get card number / args: KNN - classifier, numpy array - card / return: string - color
 def getCardNumber(clf, card):
-    number = "number"
-    #prediction = clf.predict(card.data)
-    #number += str(cardNumbers[prediction[0]])
+    number = ""
+    prediction = clf.predict(card.data)
+    number += str(cardNumbers[prediction[0]])
     return number
 
 #get card data / args: numpy array - image / return: numpy array - image, string - card info
 def getCardData(frame):
-    #cardCrop = frame[225:590, 225:470]
-    #colorCrop = cardCrop[100:101, 100:101]
-    #numberCrop = cardCrop[20:90, 23:73] #70x50 corner ML not square crop
-    #numberCrop = cardCrop[110:260, 50:200] #150x150 middle ML square crop
     cardData = ""
     
-    #img processing
-    frame = np.asarray(frame)
-    mono = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
-    blur = cv.blur(mono, (10, 10))
-    th = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 35,2)
-    kernel = np.ones((5, 5), np.uint8)
-    close = cv.morphologyEx(th, cv.MORPH_OPEN, kernel)
-    #canny = cv.Canny(close, 100, 200)
-    cont, t = cv.findContours(close, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    #card in image
+    cardCont = getContours(frame)
+    cnt = ()
+    maxCnt = 1600
+    for c in cardCont: 
+        if len(c) > len(cnt) and len(c) < maxCnt: cnt = c
+    (x,y),(MA,ma),angle = cv.fitEllipse(cnt)
+    
+    #rotate frame
+    (h, w) = frame.shape[:2]
+    center = (x, y)
+    angle = 360 - (180 - angle)
+    scale = 1
+    M = cv.getRotationMatrix2D(center, angle, scale)
+    rFrame = cv.warpAffine(frame, M, (w, h))
+    
+    #card in rotated image
+    rCardCont = getContours(rFrame)
+    rCnt = ()
+    maxCnt = 1600
+    for c in rCardCont: 
+        if len(c) > len(rCnt) and len(c) < maxCnt: rCnt = c
+    (x,y),(MA,ma),angle = cv.fitEllipse(cnt)
+    
+    #crops
+    ch = 360
+    cw = 240
+    cx = int((x - (ch-120)/2))
+    cy = int((y - (cw+120)/2))
+    cardCrop = rFrame[cx:cx+ch, cy:cy+cw]
+    #numberCrop = cardCrop[15:95, 15:95] 80x80 corner
+    numberCrop = cardCrop[110:260, 50:200]
+    colorCrop = cardCrop[100:102, 150:152]
+    
+    #number in card
+    numberCrop = getThreshold(numberCrop)
+   
+    print(plt.imshow(frame))
+    plt.pause(0.001)
+    print(plt.imshow(rFrame))
+    plt.pause(0.001)
+    print(plt.imshow(cardCrop))
+    plt.pause(0.001)
+    print(plt.imshow(colorCrop))
+    plt.pause(0.001)
+    print(plt.imshow(numberCrop, cmap='Greys_r'))
+    plt.pause(0.001)
     
     #predict
-    pc = Bunch(data=np.array(frame))
-    pc = (pc.data).reshape(1, resX*resY*3)
-    col = getCardColor(colorClf, pc)
-    num = str(getCardNumber(numberClf, pc))
-    cardData = col + num
+    pcol = Bunch(data=colorCrop)
+    pcol = (pcol.data).reshape(1, 2*2*3)
+    col = getCardColor(colorClf, pcol)
+    pnum = Bunch(data=numberCrop)
+    pnum = (pnum.data).reshape(1, 150*150)
+    num = str(getCardNumber(numberClf, pnum))
+    cardData = num + " " + col
    
     #draw
-    cnt = ()
-    maxCnt = int(resY*4)
-    for c in cont: 
-        if len(c) > len(cnt) and len(c) < maxCnt: cnt = c
-    x,y,w,h = cv.boundingRect(cnt)
+    x,y,w,h = cv.boundingRect(np.asarray(cnt))
     cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 153), 2)
-    cv.putText(frame, cardData, (x+5, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 153), 2)
+    cv.putText(frame, cardData, (x+5, y-10), cv.FONT_HERSHEY_SIMPLEX, 1, (38, 38, 38), 2)
+    
     return [frame, cardData]
 
 #training models for KNN classifiers
@@ -75,24 +127,27 @@ def trainModels():
     #color
     dc = []
     tc = []
-    for c in cardColors:
+    for n in cardNumbers:
         t = []
-        for n in cardNumbers:
+        for c in cardColors:
             try:
                 el = np.asarray(load('img/'+ c + str(n) +'.jpg', target_size=(resX,resY)))
-                dc.append(el)
+                cardCrop = el[225:590, 225:470]
+                colorCrop = cardCrop[100:102, 100:102]
+                #print(plt.imshow(colorCrop))
+                #plt.pause(0.001)
+                dc.append(colorCrop)
                 t.append(cardColors.index(c))
             except IOError: break
         tc += t
     colD = Bunch(data=np.array(dc), target=np.array(tc))
-    X = colD.data.reshape(len(dc), resX*resY*3)
+    X = colD.data.reshape(len(dc), 2*2*3)
     y = colD.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, train_size=.8, random_state=42)
     print(np.shape(X_train), np.shape(X_test), len(y_train), len(y_test))
     colorClf.fit(X_train, y_train)
-    trainAcc = colorClf.score(X_train, y_train)
-    testAcc = colorClf.score(X_test, y_test)
-    print("   train-acc " + str(trainAcc) + "       test-acc " + str(testAcc) )
+    colorTrainAcc = colorClf.score(X_train, y_train)
+    colorTestAcc = colorClf.score(X_test, y_test)
         
     #numbers
     dn = []
@@ -102,19 +157,25 @@ def trainModels():
         for c in cardColors:
             try:
                 el = np.asarray(load('img/'+ c + str(n) +'.jpg', target_size=(resX,resY)))
-                dn.append(el)
+                cardCrop = el[225:590, 225:470]
+                numberCrop = getThreshold(cardCrop[110:260, 50:200])
+                #print(plt.imshow(numberCrop, cmap='Greys_r'))
+                #plt.pause(0.001)
+                dn.append(numberCrop)
                 t.append(cardNumbers.index(n))
             except IOError: break
         tn += t
     numD = Bunch(data=np.array(dn), target=np.array(tn))
-    X = colD.data.reshape(len(dc), resX*resY*3)
+    X = numD.data.reshape(len(dc), 150*150)
     y = numD.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, train_size=.8, random_state=42)
     print(np.shape(X_train), np.shape(X_test), len(y_train), len(y_test))
     numberClf.fit(X_train, y_train)
-    trainAcc = numberClf.score(X_train, y_train)
-    testAcc = numberClf.score(X_test, y_test)
-    print("   train-acc " + str(trainAcc) + "       test-acc " + str(testAcc) )
+    numberTrainAcc = numberClf.score(X_train, y_train)
+    numberTestAcc = numberClf.score(X_test, y_test)
+    
+    print("   train-acc " + str(colorTrainAcc) + "       test-acc " + str(colorTestAcc) )
+    print("   train-acc " + str(numberTrainAcc) + "       test-acc " + ('{:.2f}'.format(numberTestAcc)) )
    
   
 
--- main.py
+++ main.py
@@ -1,19 +1,88 @@
 #main file
 import sys
+import os
+import cv2 as cv
+import numpy as np
 import matplotlib.pyplot as plt
-from menu import *
-from core import readFromFile
-from core import liveStream
 from data_capturing import collectData
 from image_processing import trainModels
+from image_processing import getCardData
+from keras.preprocessing.image import load_img as load 
 
 cardColors = ["r", "g", "b", "y"]
 cardNumbers = [0,1,2,3,4,5,6,7,8,9,"d","a","n"]
 
+#MENU
+#menu
+def getUserOption():
+    print("")
+    print ("-----------------")
+    print ("*** Uno Cards ***")
+    print ("-----------------")
+    print("[1] Read from file")
+    print("[2] Live camera stream")
+    print("[3] Data capturing")
+    print("[0] Exit")
+    option = input("::")
+    return option
 
+#def read from file submenu
+def getUserReadSubMenuOption():
+    print("[1] Select card")
+    print("[2] All data")
+    print("[9] Return")
+    option = input("::")
+    return option
+
+#camIndex
+def getUserCamIndex():
+    print("Please input CAMERA INDEX: 0, 1, 2 etc")
+    camIndex = input("::")
+    return int(camIndex)
+
+#card selection
+def getUserCard():
+    print("PLease input COLOR: r,g,b,y")
+    userCard = input("::")
+    print("Please input NUMBER: 0,1,2,3,4,5,6,7,8,9,d,a,n")
+    userCard += input("::")
+    return userCard 
+
+#CORE
+#live stream / args: int - camera index
+def liveStream(camIndex):
+    print("Opening camera with index " + str(camIndex) + "...")
+    vc = cv.VideoCapture(camIndex)
+    vc.set(3, 800)
+    vc.set(4, 600)
+    i = 0
+    while vc.isOpened():
+        rval, frame = vc.read()
+        if i > 20:
+            frame = np.asarray(cv.cvtColor(frame, cv.COLOR_BGR2RGB))
+            frame, detectedCard = getCardData(frame)
+            frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+        cv.imshow("stream", frame)
+        i+=1
+        key = cv.waitKey(1)
+        if key == 27: break
+    cv.destroyWindow("stream")
+    vc.release()
+
+#form file / args: string - image name
+def readFromFile(imgName):
+    if os.path.isfile("img/" + imgName + ".jpg"):
+        frame = np.asarray(load('img/'+ imgName +'.jpg', target_size=(600,800)))
+        print("Card Info: " + str(imgName))
+        enhancedFrame, detectedCard = getCardData(frame)
+        print(plt.imshow(frame))
+        print("Detected as: " + str(detectedCard))
+        plt.pause(0.001)
+
+#PROGRAM
 #train ml models
 print("Initializing ML models..")
-trainModels() 
+trainModels()
 
 #main loop
 opt = getUserOption()
